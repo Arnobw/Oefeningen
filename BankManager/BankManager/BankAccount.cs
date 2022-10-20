dc9a6c7310cb2907465c5adfa558ffece8b92af8
arnobw <arnobw@hotmail.com>
2022-10-20 18:17:19 +0200

    banken enal

---
+++ BankManager/BankManager/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManager
+{
+    public class BankAccount
+    {
+        public string name;
+        public double funds;
+        public string number;
+
+        public BankAccount(string name, double funds, string number)
+        {
+            this.name = name;
+            this.funds = funds;
+            this.number = number;
+
+        }
+
+        public double WithDrawFunds(double amount)
+        {
+
+            if (funds < amount)
+            {
+
+                Console.WriteLine("NOT ENOUGH FUNDS TO WITHDRAW SPECIFIED AMOUNT", Console.ForegroundColor = ConsoleColor.Red);
+
+                double leftover = amount - funds;
+                amount = - leftover;
+                funds = -amount;
+                return leftover;
+
+            } else
+
+            funds = funds - amount;
+            return amount;
+        }
+
+            public double AddFunds(double amount)
+        {
+
+            funds += amount;
+            return amount;
+        }
+
+        public double GetBalance() {
+            return this.funds;
+        }
+
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{name}, {funds}, {number}");
+        }
+    }
+
+
+
+}
---
+++ BankManager/BankManager/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankManager;
+
+BankAccount acc = new BankAccount("arno", 5236, "BE02 6587 5654 4115");
+
+acc.PrintInfo();
+
+Console.WriteLine(acc.GetBalance());
+
+
+Console.WriteLine($"{acc.GetBalance()}EUR + {acc.AddFunds(1000)}EUR =  {acc.GetBalance()}EUR");
+
